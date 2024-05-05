220d350a96a08d1b41c068d4986d66ec0e51067b
Macedo Francisco <macedolb53@gmail.com>
2024-05-05 14:31:56 +0200

    colletion map

---
+++ JS/Exercicios/ex04.js
@@ -0,0 +1,27 @@
+const mixValores = [2,4,5,6, ["2",9,"10"], Object[30]];
+
+mixValores.forEach((e)=>{
+    let soma =0;
+    if(Array.isArray()){
+        return soma
+    }else if(Array.isArray(Number)){
+        
+    }
+    soma += e
+    console.log(e)
+    console.log(soma)
+})
+// const num = Array(2,5,6,8,10,12,[["20"]]);
+// const  n = num.filter((v)=>{
+//     let soma =0;
+
+//     if(v % 2 == 0){
+//         soma += v
+        
+//     }
+//     console.log(soma)
+
+// })
+// console.log(n)
+
+
