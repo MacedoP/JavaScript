379806ae3de341dc1b7a2b2547ed9ee42fe16ebd
Francisco Macedo <macedolb53@gmail.com>
2024-05-08 17:18:28 +0200

    Create ex01_Introducao.js

---
+++ JS/Novo Modulo/Objectos/ex01_Introducao.js
@@ -0,0 +1,15 @@
+//Nesta aulas vamos falar sobre objetos em JavaScript, na pratica orientacao a objetos
+
+
+class Pessoa{
+
+
+    constructor(){
+        this.nome = "Macedo";
+    }
+}
+
+let p1 = new Pessoa();
+console.log(p1.nome);
+
+
