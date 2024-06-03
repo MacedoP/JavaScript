5c02583d4a044edfbd0ba6494123ccc11a05d5ea
Macedo Francisco <macedolb53@gmail.com>
2024-06-03 19:52:57 +0200

    Create relacionasis.js

---
+++ JS/Exercicios/relacionasis.js
@@ -0,0 +1,23 @@
+/*
+Revisa sobre variaveis={let , var, const}
+Operadores Relacionas={>,<, >=, <=, ==, ===, !=}
+Extrutura Condicionais={if, switch case}
+
+*/
+var nome = "Francisco Macedo Paulo";
+var idade = 28;
+var sexo = "M";
+var altura = 1.75;
+var peso = "75.kg";
+
+// console.log `O seu nome é ${nome} 
+//             tens ${idade} de idade es do sexo 
+//             ${sexo} tens uma alturA de 
+//             ${altura} com um peso de 
+//             ${peso}`;
+
+let valor1 = 20;
+let valor2 = 10;
+
+const res = (valor1 === valor2)? "Maior" : "Menor";
+console.log(res)
